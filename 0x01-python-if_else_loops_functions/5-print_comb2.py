433d9f8b5fa7e7a7f9839d73b4a12ddf0ebe7718
Nwafor <nwaforemmanuel42@gmail.com>
2022-06-29 00:10:25 +0000

    task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for num in range(100):
-    print("{2.:}".format(num), end=', ')
     if num == 99:
-        print("\n")
+        print("99")
+    else:
+        print("{:02d}, ".format(num), end='')
