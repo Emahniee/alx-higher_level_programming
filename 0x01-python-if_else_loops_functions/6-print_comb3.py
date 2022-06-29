41dd14257f151203c8755914f5d529dff620d100
Nwafor <nwaforemmanuel42@gmail.com>
2022-06-29 21:32:34 +0000

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for n1 in range(0, 10):
-    for n2 in range(n1 + 1, 10):
-        print("{:d}{:d}".format(n1, n2),
-              end='\n' if n1 == 8 and n1 + 1 == 9 else ', ')
+for a in range(10):
+    for b in range(a + 1, 10):
+        if a == 8 and b == 9:
+            print("{}{}".format(a, b), end='\n')
+        else:
+            print("{}{},".format(a, b), end=' ')
