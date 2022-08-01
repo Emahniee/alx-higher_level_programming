b7cd4696784b9bd56b114fc574154655ae0e3f66
Nwafor <nwaforemmanuel42@gmail.com>
2022-08-01 15:20:11 +0000

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+''' Module for task 1'''
+
+
+class MyList(list):
+    '''Subclass of list'''
+
+
+    def print_sorted(self):
+        '''prints list in sorted ascending order'''
+        print(sorted(list))
