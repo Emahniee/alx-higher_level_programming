1381172b121c5abd36e9abcf646b843b83dd088c
Nwafor <nwaforemmanuel42@gmail.com>
2022-08-01 18:11:10 +0000

    task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Module for task 2'''
+
+
+
+def is_same_class(obj, a_class):
+    '''Checking if object is exactly an instance of the specified class'''
+    if obj == a_class:
+        return True
+    else:
+        return False
