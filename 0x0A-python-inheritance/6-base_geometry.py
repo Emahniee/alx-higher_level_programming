0447e6a1a55637af9f6efe8e2ed98f3867f68952
Nwafor <nwaforemmanuel42@gmail.com>
2022-08-01 19:56:57 +0000

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/urs/bin/python3
+'''Module for task 6'''
+
+
+class BaseGeometry():
+    '''Class BaseGeometry'''
+    def area(self):
+        '''Method not Implemented'''
+        raise Exception("area() is not implemented")
