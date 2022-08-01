9027078b9ebc5206973099fa67a17b982eeb4d8f
Nwafor <nwaforemmanuel42@gmail.com>
2022-08-01 18:28:17 +0000

    task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Module for task 3'''
+
+
+
+def is_kind_of_class(obj, a_class):
+    '''True if the object is an instance of current class, or a class current class inherited from'''
+    return type(obj) == a_class
