2091f88e8bd90ddad16ca5ab0a44aff08ec8badf
Nwafor <nwaforemmanuel42@gmail.com>
2022-08-02 20:36:59 +0000

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     '''Read the contents of a file and print to stdout'''
-    with open(filename, encoding="uft-8") as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
