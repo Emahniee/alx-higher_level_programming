e6e05c5a820ba53d8303ab2f37961c86bd52a056
Nwafor <nwaforemmanuel42@gmail.com>
2022-08-02 21:05:36 +0000

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     '''A function that writes a string to a text file and returns the number of characters written'''
-    with open(filename, w, encoding="utf=-8") as f:
+    with open(filename, 'w', encoding="utf-8") as f:
         return f.write(text)
