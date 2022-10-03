d14fd73c25b19171d21b218dac839d14a5984bb8
Nwafor <nwaforemmanuel42@gmail.com>
2022-10-03 20:50:40 +0000

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-
 if (process.argv[3]) {
-	console.log('Arguments found');
-}
-else if (process.argv[2]) {
-	console.log('Arguments found');
-}
-else {
-	console.log('No Arguments');
+  console.log('Arguments found');
+} else if (process.argv[2]) {
+  console.log('Argument found');
+} else {
+  console.log('No argument');
 }
