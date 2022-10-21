f52ed22ded98cc283c37b727300aa0d28e1cfed9
Nwafor <nwaforemmanuel42@gmail.com>
2022-10-21 21:06:59 +0000

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-""" Fetches https://intranet.hbtn.io/status"""
+"""
+Script that fetches https://alx-intranet.hbtn.io/status
+"""
+
 import requests
 
-
 if __name__ == '__main__':
-    s = requests.Session()
-
-    response = s.get('https://intranet.hbtn.io/status')
-    body = response.text
-
+    response = requests.get('https://alx-intranet.hbtn.io/status')
     print('Body response:')
-    print('\t- type: {}'.format(type(body)))
-    print('\t- content: {}'.format(body))
+    print('\t- type: {}'.format(type(response.text)))
+    print('\t- content: {}'.format(response.text))
